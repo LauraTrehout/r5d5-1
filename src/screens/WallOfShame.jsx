d0e0c8dfea7ea4fd270f912a9311157e7270ca6c
LauraTrehout <lauratrehout@gmail.com>
2021-08-06 00:40:11 +0200

    active style on navbar and created wookie button

--- src/screens/WallOfShame.jsx
+++ src/screens/WallOfShame.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Header from './../components/Header';
+import NavBar from './../components/NavBar';
+
+import './WallOfShame.css';
+
 const WallOfShame = () => {
-    return 
- <div>
- </div>
+    return (
+        <>
+            <Header />
+            <NavBar />
+            
+
+        </>
+    )
 }
 
 export default WallOfShame;
