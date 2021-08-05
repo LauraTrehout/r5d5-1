67e715bd60cf299f04bd8bc97107324fdcd56c2b
Axel Gourlin <gourlin.axel@gmail.com>
2021-08-05 23:35:22 +0200

    add popup connexion

--- src/screens/WelcomePage.jsx
+++ src/screens/WelcomePage.jsx
@@ -1,26 +1,16 @@
-import React, {useState} from 'react';
 
-import { Modal } from './Modal';
+import Connexion from '../components/Connexion';
+import ReactCircleModal from 'react-circle-modal'
 
 import "tailwindcss/tailwind.css";
+import './WelcomePage.css';
 
 import leiaSlave from '../assets/leia-slave-no-bg.png';
 
 const WelcomePage = () => {
 
-  const [showModal, setShowModal] = useState(false);
 
 
-  const openModal = () => {
-    setShowModal(true);
-  };
-  // return (
-  //   <div className="App">
-  //     <h1>Popup Modal</h1>
-  //     <button onClick={openModal}>Open Modal</button>
-  //     {showModal ? <Modal setShowModal={setShowModal} /> : null}
-  //   </div>
-  // );
 
   return (
     <div class='w-full h-screen text-center bg-accueil bg-center bg-origin-border bg-no-repeat bg-cover flex flex-col justify-between'>
@@ -28,10 +18,28 @@ const WelcomePage = () => {
       >STARLOVE</h1>
       <img src={leiaSlave} alt="Img of Leia" style={{ margin: '0 auto 25px auto', maxWidth: '600px'}} />
       <div class="mb-20 space-x-20">
-        <button class="w-40 px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md" onClick={openModal}>Se Connecter</button>
-        <button class="w-40 px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md" >Inscription</button>
+        <ReactCircleModal
+      backgroundColor="rgba(0,0,0,0.5)"
+      toogleComponent={onClick => (
+        <button class="w-40 px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md" onClick={onClick}>
+          Se Connecter
+        </button>
+      )}
+      // Optional fields and their default values
+      offsetX={0}
+      offsetY={0}
+    >
+      {(onClick) => (
+        <div className="modal" style={{ padding: '1em' }}>
+          <Connexion />
+          <button className='btnClose'  onClick={onClick}>
+            Close
+          </button>
+        </div>
+      )}
+        </ReactCircleModal>
+        <button class="w-40 px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md">Inscription</button>
       </div>
-      {showModal ? <Modal setShowModal={setShowModal} /> : null}
     </div>
   );
 }
