14300adde3380b8407a0b8098a48a09fcf0b91e7
Axel Gourlin <gourlin.axel@gmail.com>
2021-08-05 21:58:49 +0200

    add color tailwind

--- src/screens/WelcomePage.jsx
+++ src/screens/WelcomePage.jsx
@@ -7,12 +7,12 @@ import leiaSlave from '../assets/leia-slave-no-bg.png';
 const WelcomePage = () => {
   return (
     <div class='w-full h-screen text-center bg-accueil bg-center bg-origin-border bg-no-repeat bg-cover flex flex-col justify-between'>
-      <h1 class="mt-14 text-8xl font-bold text-pink-500" style={{ textShadow: '-2px 2px 1px #000, 2px 2px 1px #000, 2px -2px 1px #000,-2px -2px 1px #000, 0 20px 15px rgb(236, 72, 153)'}}
+      <h1 class="mt-14 text-8xl font-bold text-pinka" style={{ textShadow: '-2px 2px 1px #000, 2px 2px 1px #000, 2px -2px 1px #000,-2px -2px 1px #000, 0 20px 15px rgb(236, 72, 153)'}}
       >STARLOVE</h1>
       <img src={leiaSlave} alt="Img of Leia" style={{ margin: '0 auto 25px auto', maxWidth: '600px'}} />
       <div class="mb-20 space-x-20">
-        <button class="px-4 py-2 border-4 border-black font-bold text-lg bg-pink-500 rounded-md">Se Connecter</button>
-        <button class="px-4 py-2 border-4 border-black font-bold text-lg bg-pink-500 rounded-md" >Inscription</button>
+        <button class="px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md">Se Connecter</button>
+        <button class="px-4 py-2 border-4 border-black font-bold text-lg bg-pinka rounded-md" >Inscription</button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,13 @@ module.exports = {
         DEFAULT: '#2a1d31',
         dark: '#009eeb',
       },
+      pinka: {
+        light: '#EACBF9',
+        DEFAULT: '#DAA2F4',
+        dark: '#8415B7',
+      },
     },
-      
+
     extend: {
       backgroundImage: theme => ({
         'hello-there': "url('/src/assets/hello-there.png')",
