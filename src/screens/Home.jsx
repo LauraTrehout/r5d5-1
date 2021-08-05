6d8081f2936b82cc40bef032c7f5ddca33c04766
fdmesquita <fannydemesquita@gmail.com>
2021-08-06 00:59:30 +0200

    delete connexion composant from home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PickYourSide from "../components/PickYourSide";
 import NavBar from "../components/NavBar";
 import Header from "../components/Header";
-import Connexion from "../components/Connexion";
 
 const Home = () => {
   return (
@@ -10,7 +9,6 @@ const Home = () => {
       <Header />
       <NavBar />
       <PickYourSide />
-      <Connexion />
     </div>
   );
 };
