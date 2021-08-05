63eaa257175982146e68e49d7c141ab49062c1cc
tristanfluteaux <tristan.fluteaux@gmail.com>
2021-08-05 19:11:52 +0200

    create Header and NavBar with some css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import MyProfile from '../screens/MyProfile';
 import Search from '../screens/Search';
 
 const NavBar = () => {
     return (
-        <div class='h-11 m- 11 bg-purple-400 flex justify-start'>
-            <div>
-                <button>Profile</button>
+        <div class='flex' style={{backgroundColor:'#91709b'}}>
+            <div class='m-5 text-white' style={{fontSize:'30px'}}>
+                <Link to='/MyProfile'><button>Profile</button></Link>
             </div>
-            <div>
-                <button>Search</button>
+            <div class='m-5 text-white' style={{fontSize:'30px'}}>
+                <Link to='/Search'><button>Search</button></Link>
             </div>
-            <div>
-                <button>Wall of Shame</button>
+            <div class='m-5 text-white' style={{fontSize:'30px'}}>
+                <Link to='/WallOfShame'><button class=''>Wall of Shame</button></Link>
             </div>
         </div>
 
