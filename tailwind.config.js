bc8b9e7bc17ff5e340709b126ad4b53ba452e07b
Benjamin1er <benjaminj.geron@gmail.com>
2021-08-05 19:13:03 +0200

    Started PickYourSide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      lightside: {
+        light: '#85d7ff',
+        DEFAULT: '#f39819',
+        dark: '#009eeb',
+      },
+      darkside: {
+        light: '#85d7ff',
+        DEFAULT: '#2a1d31',
+        dark: '#009eeb',
+      },
+    },
+      
     extend: {
       backgroundImage: theme => ({
         'hello-there': "url('/src/assets/hello-there.png')",
