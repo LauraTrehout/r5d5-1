23264ef26bcfe6a370c06480b473a19db47fd927
fdmesquita <fannydemesquita@gmail.com>
2021-08-05 23:36:57 +0200

    add errror to connexion

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         DEFAULT: "#2a1d31",
         dark: "#009eeb",
       },
-      greypurple: {
+      graypurple: {
         light: "#393E41",
         DEFAULT: "#393e41",
         dark: "#000000",
@@ -23,13 +23,28 @@ module.exports = {
       purple: {
         light: "#B8A2BE",
         DEFAULT: "#91709b",
-        dark: "#684E70",
+        dark: "#49364E",
       },
-      // purple: {
-      //   light: '#B8A2BE',
-      //   DEFAULT: '#91709b',
-      //   dark: '#684E70',
-      // },
+      orange: {
+        light: "#F6AF4C",
+        DEFAULT: "#f39819",
+        dark: "#CA7A0B",
+      },
+      gray: {
+        light: "#F6AF4C",
+        DEFAULT: "#d3d0cb",
+        dark: "#B7B3AB",
+      },
+      lightgray: {
+        light: "#ECEBE6",
+        DEFAULT: "#e7e5df",
+        dark: "#BDB8A8",
+      },
+      error: {
+        light: "#FF5B5B",
+        DEFAULT: "#ff0000",
+        dark: "#920000",
+      },
     },
 
     extend: {
