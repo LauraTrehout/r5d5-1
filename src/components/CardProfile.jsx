aa9beba86d4e31905e537a8885f0983515f23936
LauraTrehout <lauratrehout@gmail.com>
2021-08-05 23:04:21 +0200

    dark and light side css ok

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './CardProfile.css';
 
 const CardProfile = ({ id, name, image }) => {
-    return (
-        <div className="card-profile">
-          <h3>{name}</h3>
-          <img src={image} className='card-img-dark' />
-        </div>
-    )
+  return (
+    <div className="card-profile">
+      <h3>{name}</h3>
+      <Link to={`/profile/${id}`}>
+        <img src={image} className='card-img-dark' />
+      </Link>
+    </div>
+  )
 }
 
 export default CardProfile;
