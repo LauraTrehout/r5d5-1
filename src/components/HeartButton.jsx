e844447446449f82b53df2395f94008a14fed408
fdmesquita <fannydemesquita@gmail.com>
2021-08-06 08:49:26 +0200

    add tailwind style, wip heartbutton

--- src/components/HeartButton.jsx
+++ src/components/HeartButton.jsx
@@ -1,31 +1,51 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const HeartButton = () => {
-  const [like, setLike] = useState({
-    open: false,
-    fav: false,
-    Transition: Slide,
-  });
+  //   const [like, setLike] = useState({
+  //     open: false,
+  //     fav: false,
+  //     Transition: Slide,
+  //   });
 
-  const handleClick = (Transition) => () => {
-    setLike({
-      open: true,
-      fav: !like.fav,
-      Transition,
-    });
-  };
+  //   const handleClick = (Transition) => () => {
+  //     setLike({
+  //       open: true,
+  //       fav: !like.fav,
+  //       Transition,
+  //     });
+  //   };
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setLike({
-      ...like,
-      open: false,
-    });
-  };
+  //   const handleClose = (event, reason) => {
+  //     if (reason === "clickaway") {
+  //       return;
+  //     }
+  //     setLike({
+  //       ...like,
+  //       open: false,
+  //     });
+  //   };
 
-  return <div className='heart-icon'></div>;
+  return (
+    <div className='heart-icon'>
+      <button class='absolute top-0 '>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          class='h-6 w-6'
+          fill='false'
+          hover='true'
+          viewBox='0 0 24 24'
+          stroke='currentColor'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='2'
+            d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+          />
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default HeartButton;
--- src/screens/CardProfileDetails.jsx
+++ src/screens/CardProfileDetails.jsx
@@ -1,73 +1,77 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Header from '../components/Header';
-import NavBar from '../components/NavBar';
+import Header from "../components/Header";
+import HeartButton from "../components/HeartButton";
+import NavBar from "../components/NavBar";
 
-import './CardProfileDetails.css'
+import "./CardProfileDetails.css";
 
 const CardProfileDetails = () => {
+  const [details, setdetails] = useState([]);
+  const { id } = useParams();
 
-    const [details, setdetails] = useState([]);
-    const { id } = useParams();
+  useEffect(() => {
+    const LoveDetail = () => {
+      axios
+        .get(`https://miadil.github.io/starwars-api/api/id/${id}.json`)
+        .then((result) => setdetails(result.data));
+    };
 
-    useEffect(() => {
-        const LoveDetail = () => {
-            axios
-                .get(`https://miadil.github.io/starwars-api/api/id/${id}.json`)
-                .then((result) => setdetails(result.data)
-                );
-        }
-
-        LoveDetail();
-    }, []);
-    return (
-        <>
-            <Header />
-            <NavBar />
-            <Link to='/WallOfShame'>
-            <button className='wookie-btn'>#BalanceTonWookie</button>
-            </Link>
-            <div className='details-profile'>
-                <div className='details-image'>
-                    <img className='d-image' src={details.image} alt={details.name} />
-                </div>
-                <div className='details-character'>
-                    <div className='semple'>
-                        <div className='details-character-1'>
-                            <p>eye color : {details.eyeColor}</p>
-                            <p>age : {details.born}</p>
-                            <p>height : {details.height}</p>
-                        </div>
-                        <div className='details-character-1'>
-                            <p>homeworld : {details.homeworld}</p>
-                            <p>gender : {details.gender}</p>
-                            <p>species : {details.species}</p>
-                        </div>
-                    </div>
-                    <div className='details-yellowbox'>
-
-                        <div className='details-yellowbox-inside'>
-                            <h4>{details.name}</h4>
-                                <br/>
-                            <p>Masters : {details.masters}</p>
-                                <br/>
-                        <p>Apprentices : {details.apprentices}</p>
-                            <br/>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti autem, alias
-                         quo possimus numquam quis, aspernatur quia, unde reprehenderit dignissimos quisquam. 
-                         At molestias voluptatum accusamus voluptates aliquid consequuntur expedita eligendi.
-                        </p>
-                        </div>              
-                </div>
+    LoveDetail();
+  }, []);
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Link to='/WallOfShame'>
+        <button className='wookie-btn'>#BalanceTonWookie</button>
+      </Link>
+      <div className='details-profile'>
+        <div className='details-image'>
+          <div class='relative'>
+            <HeartButton />
+            <img className='d-image' src={details.image} alt={details.name} />
+          </div>
+        </div>
+        <div className='details-character'>
+          <div className='semple'>
+            <div className='details-character-1'>
+              <p>eye color : {details.eyeColor}</p>
+              <p>age : {details.born}</p>
+              <p>height : {details.height}</p>
             </div>
-        </>
-
-    )
+            <div className='details-character-1'>
+              <p>homeworld : {details.homeworld}</p>
+              <p>gender : {details.gender}</p>
+              <p>species : {details.species}</p>
+            </div>
+          </div>
+          <div className='details-yellowbox'>
+            <div className='details-yellowbox-inside'>
+              <h4>{details.name}</h4>
+              <br />
+              <p>Masters : {details.masters}</p>
+              <br />
+              <p>Apprentices : {details.apprentices}</p>
+              <br />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Corrupti autem, alias quo possimus numquam quis, aspernatur
+                quia, unde reprehenderit dignissimos quisquam. At molestias
+                voluptatum accusamus voluptates aliquid consequuntur expedita
+                eligendi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CardProfileDetails;
