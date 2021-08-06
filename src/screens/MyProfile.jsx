d5169a7afb40dec473b7e65c79e02a6269e681f0
Axel Gourlin <gourlin.axel@gmail.com>
2021-08-06 11:14:00 +0200

    add form bool

--- src/screens/MyProfile.jsx
+++ src/screens/MyProfile.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import Header from '../components/Header'
-import NavBar from '../components/NavBar'
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
 import iLoveYou from '../assets/loveLukeLeia.png';
 import './MyProfile.css';
 
 const MyProfile = () => {
+
+  const [modified, setModified] = useState(true);
+  const [message, setMessage] = useState("");
+  const [myProfile, setMyprofile] = useState({
+    name: "",
+    email: "",
+    gender: "",
+    species: "",
+    eyeColor: "",
+    height: "",
+  })
+
+  const { name, email, gender, species, eyeColor, height } = myProfile;
+
+  const change = (e) => {
+    setMyprofile({ ...myProfile, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    setMessage("");
+    e.preventDefault();
+
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("gender", gender);
+    sessionStorage.setItem("species", species);
+    sessionStorage.setItem("eyeColor", eyeColor);
+    sessionStorage.setItem("height", height);
+
+    setMessage("Information modified !");
+  };
 
   return (
     <>
@@ -17,19 +48,51 @@ const MyProfile = () => {
           <h2>My Profile</h2>
           <img src={iLoveYou} alt="Img kiss" />
         </div>
-        {/* <form action="">
-          <input type="text" name="name" id="" />
-          <input type="email" name="holoMail" id="" />
-          <select name="gender" id="">
-            <option value="Woman"></option>
-          </select>
-          <select name="species" id="">
-            <option value="Droide"></option>
-          </select>
-          <input type="text" name="eyeColor" id="" />
-          <input type="date" name="age" id="" />
-          <input type="number" name="heigth" id="" />
-        </form> */}
+        { modified ? <div className="myInfoContainer">
+          <form className="myInfo" onSubmit={submit}>
+
+          <div>
+            <label htmlFor="name"><span>Name : </span> </label>
+            <input type="text" name="name" id="name" />
+          </div>
+
+          <div>
+            <label htmlFor="mail"><span>Email : </span> </label>
+            <input type="email" name="mail" id="mail" />
+          </div>
+
+          <div>
+            <label htmlFor="gender"><span>Gender : </span> </label>
+            <select name="gender" id="gender">
+              <option value="Woman">Woman</option>
+              <option value="Man">Man</option>
+              <option value="A thing with many Hair">A thing with many Hair</option>
+              <option value="A thing with many tentacles">A thing with many tentacles</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="species"><span>Species : </span> </label>
+            <select name="species" id="species">
+              <option value="Droide">value</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="eyeColor"><span>Eye color : </span> </label>
+            <input type="text" name="eyeColor" id="eyeColor" />
+          </div>
+
+          <div>
+            <label htmlFor="heigth"><span>Heigth : </span> </label>
+            <input type="number" name="heigth" id="heigth" />
+          </div>
+
+
+          </form>
+          <button type='submit' name='submit'>Valider</button>
+        </div>
+        :
           <div className="myInfoContainer">
             <div className="myInfo">
               <p><span>Name : </span> Leia Organa</p>
@@ -39,8 +102,9 @@ const MyProfile = () => {
               <p><span>Eye Color : </span> Brown</p>
               <p><span>Heigth : </span> 1m50</p>
             </div>
-            <button>Modifier</button> {/* button submit */}
+            <button>Modifier</button>
           </div>
+        }
       </div>
     </>
   );
