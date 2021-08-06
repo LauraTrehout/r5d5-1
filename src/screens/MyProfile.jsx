d0ab0121da9ad67b3f09c7f449a6614ce9ef50e3
Axel Gourlin <gourlin.axel@gmail.com>
2021-08-06 12:42:00 +0200

    add modif profil

--- src/screens/MyProfile.jsx
+++ src/screens/MyProfile.jsx
@@ -36,8 +36,17 @@ const MyProfile = () => {
     sessionStorage.setItem("eyeColor", eyeColor);
     sessionStorage.setItem("height", height);
 
-    setMessage("Information modified !");
+    localStorage.setItem('myProfile', JSON.stringify(myProfile));
+
+    // setMessage("Information modified !");
+
+    setModified(false)
+    // setTimeout(() => {
+    // },1000)
   };
+
+  const profil = JSON.parse(localStorage.getItem('myProfile') || '[]')
+
 
   return (
     <>
@@ -53,17 +62,17 @@ const MyProfile = () => {
 
           <div>
             <label htmlFor="name"><span>Name : </span> </label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" placeholder='Enter Name' value={name} onChange={change}/>
           </div>
 
           <div>
-            <label htmlFor="mail"><span>Email : </span> </label>
-            <input type="email" name="mail" id="mail" />
+            <label htmlFor="email"><span>Email : </span> </label>
+            <input type="text" name="email" id="email" placeholder='Enter Holomail' value={email} onChange={change}/>
           </div>
 
           <div>
             <label htmlFor="gender"><span>Gender : </span> </label>
-            <select name="gender" id="gender">
+            <select name="gender" id="gender" value={gender} onChange={change}>
               <option value="Woman">Woman</option>
               <option value="Man">Man</option>
               <option value="A thing with many Hair">A thing with many Hair</option>
@@ -73,36 +82,47 @@ const MyProfile = () => {
 
           <div>
             <label htmlFor="species"><span>Species : </span> </label>
-            <select name="species" id="species">
-              <option value="Droide">value</option>
+            <select name="species" id="species" value={species} onChange={change}>
+              <option value="Droide">Sith</option>
+              <option value="wookiee">Wookie</option>
+              <option value="droid">Droide</option>
+              <option value="human">Human</option>
+              <option value="hutt">Hutt</option>
+              <option value="rodian">Rodian</option>
+              <option value="trandoshan">Trandoshan</option>
+              <option value="ewok">Ewok</option>
+              <option value="yoda's species">Petite grenouille des marais</option>
             </select>
           </div>
 
           <div>
-            <label htmlFor="eyeColor"><span>Eye color : </span> </label>
-            <input type="text" name="eyeColor" id="eyeColor" />
+            <label htmlFor="eyeColor"><span>Eye Color : </span> </label>
+            <input type="text" name="eyeColor" id="eyeColor" placeholder='Enter Eye color' value={eyeColor} onChange={change}/>
           </div>
 
           <div>
-            <label htmlFor="heigth"><span>Heigth : </span> </label>
-            <input type="number" name="heigth" id="heigth" />
+            <label htmlFor="height"><span>Heigth : </span> </label>
+            <input type="number" name="height" id="height" placeholder='Enter your Height' value={height} onChange={change}/>
           </div>
 
+          { message && <span>{message}</span>}
 
+          <div>
+            <button type='submit' name='submit'>Valider</button>
+          </div>
           </form>
-          <button type='submit' name='submit'>Valider</button>
         </div>
         :
           <div className="myInfoContainer">
             <div className="myInfo">
-              <p><span>Name : </span> Leia Organa</p>
-              <p><span>Email : </span> leiaStarRebelle@force.holo</p>
-              <p><span>Gender : </span> Woman</p>
-              <p><span>Species : </span> Human</p>
-              <p><span>Eye Color : </span> Brown</p>
-              <p><span>Heigth : </span> 1m50</p>
+              <p><span>Name : </span> {name}</p>
+              <p><span>Email : </span> {email}</p>
+              <p><span>Gender : </span> {gender}</p>
+              <p><span>Species : </span> {species}</p>
+              <p><span>Eye Color : </span> {eyeColor}</p>
+              <p><span>Heigth : </span> {height}</p>
+              <button onClick={() => setModified(true)}>Modifier</button>
             </div>
-            <button>Modifier</button>
           </div>
         }
       </div>
