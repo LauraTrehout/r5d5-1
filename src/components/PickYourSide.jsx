ba1b7d973d985eedb71bfaf3561a801d33ebb9df
Benjamin1er <benjaminj.geron@gmail.com>
2021-08-06 03:42:20 +0200

    I finally picked a fuckin side git add .!

--- src/components/PickYourSide.jsx
+++ src/components/PickYourSide.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
-
 import chewbacca from '../assets/chewbacca-head.png';
 import missa from '../assets/missa-middle-finger.png';
+import { Link, useHistory } from 'react-router-dom';
+import './PickYourSide.css';
+
 
 
 const Side = () => {
+
+  const history = useHistory();
+  
+  const handleClick = (event, side) => {
+    event.target.style.animation = "rotation 3s infinite ease-in";
+    event.target.style.width = "2000px"
+    event.target.style.height = "2000px"
+    event.target.style.transition = "height 5s, width 5s";
+    event.preventDefault();
+    if (side === 'light') {
+      setTimeout(() => history.push('/lightSide'), 1500);
+    } else if (side === 'dark')
+      {
+        setTimeout(() => history.push('/darkSide'), 1500);
+      }
+  }
+
     return (
-        <div class='flex'>
-          <div class='flex justify-center items-center bg-lightside w-1/2'>
-            <div class='w-full min-h'>
-              <h2 class='text-right'>PICK YO</h2>
-              <img class='justify-self-center w-30 h-52' src={chewbacca} alt='the light side' />
-              <p>Looking for a relationship</p>
+        <div className='grand-side'>
+          <div className='parent-side light'>
+            <div className='child-side light'>
+              <h2 className='pick-yo' >PICK YO</h2>
+              {/* <Link to = "/lightside" onClick={delay}> */}
+                <button onClick={(e) => handleClick(e, 'light')}>
+                  <img className='chewbacca' src={chewbacca} alt='the light side' />
+                </button>
+              {/* </Link> */}
+              <p className='relationship'>Looking for a relationship</p>
             </div>
           </div>
-          <div class='flex items-center justify-center bg-darkside w-1/2'>
-           <div class='w-full '>
-              <h2>UR SIDE</h2>
-              <img class='w-30 h-52' src={missa} alt='the dark side' />
-              <p>Looking for a one night stand</p>
+          <div className='parent-side dark'>
+           <div className='child-side dark'>
+              <h2 className='ur-side' >UR SIDE</h2>
+              <Link to = "/darkside">
+              <button onClick={(e) => handleClick(e, 'dark')}>
+                <img className='missa' src={missa} alt='the dark side' />
+              </button>
+              </Link>
+              <p className='stand'>Looking for a one night stand</p>
            </div>
           </div>        
         </div>
